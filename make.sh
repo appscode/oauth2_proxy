680d5d0f69b67b12d3ded505098d29ba3d209703
Tamal Saha <tamal@appscode.com>
2018-06-20 01:17:05 -0700

    Change image tag to 2.3.0

--- make.sh
+++ make.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 pushd $GOPATH/src/github.com/bitly/oauth2_proxy
 
-docker build -t appscode/oauth2_proxy:2.2.0 .
-docker push appscode/oauth2_proxy:2.2.0
+docker build -t appscode/oauth2_proxy:2.3.0 .
+docker push appscode/oauth2_proxy:2.3.0
 
 popd
